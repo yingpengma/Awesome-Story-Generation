375a62f50a28ef46c48b785370fa19a76003dc4e
Yingpeng MA <92796556+yingpengma@users.noreply.github.com>
2024-03-19 17:47:07 +0800

    Create getCitation.py

---
+++ scripts/getCitation.py
@@ -0,0 +1,15 @@
+import requests, json, os
+api_key = os.environ.get('API_KEY')
+headers = {'x-api-key': api_key}
+
+r = requests.post(
+    'https://api.semanticscholar.org/graph/v1/paper/batch',
+    params={'fields': 'citationCount,title'},
+    json={"ids": ["3c550bda6ac5a98f630203b1750721acc61605b2", "9ba36db0d9f2d586abaa85ea6a0b48c609c5c9ec"]}, 
+    headers=headers
+)
+
+json_data = json.dumps(r.json(), indent=2)
+# print(json_data)
+with open('citationCount.json', 'w') as file:
+    file.write(json_data)
